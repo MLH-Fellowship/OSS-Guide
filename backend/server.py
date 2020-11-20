84edf74f1b56be25bd211245d627284f3bac1444
makrandr1999 <makrand.r@somaiya.edu>
2020-11-20 16:52:43 +0530

    Add callgraph endpoint to generate call graphs

--- backend/server.py
+++ backend/server.py
@@ -34,7 +34,29 @@ def generate_uml():
         rawBytes.seek(0)
         img_base64 = base64.b64encode(rawBytes.read())
         print(img_base64)
-#        return send_file(filename+".png", mimetype='image/png')
+
+
+        return jsonify({'status': str(img_base64)})
+
+    else:
+        return "Request Method not supported"
+
+@app.route('/callgraph', methods=['POST'])
+def generate_callgraph():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if not request.data:
+            flash('Server received an empty code string.')
+            return redirect(request.url)
+        _write_to_file(request.data)
+        subprocess.call(
+            ['pycallgraph', 'graphviz', '--' ,'./code.py'])
+        img = Image.open("pycallgraph.png")
+        rawBytes = io.BytesIO()
+        img.save(rawBytes, "PNG")
+        rawBytes.seek(0)
+        img_base64 = base64.b64encode(rawBytes.read())
+        print(img_base64)
 
         return jsonify({'status': str(img_base64)})
 
