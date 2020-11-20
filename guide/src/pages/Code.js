3beab8300bf583fe0b09755f3feb289d370f3ae0
Sarthak Kundra <sarthakkundra21@gmail.com>
2020-11-20 23:19:10 +0530

    connected back-end

--- guide/src/pages/Code.js
+++ guide/src/pages/Code.js
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
 import clsx from "clsx";
+import axios from "axios";
+import base64 from "base-64";
+
 import styles from "./styles.module.css";
 
 import Editor from "../../components/Editor";
 
 const Code = () => {
+  let img;
   const [code, setCode] = useState("");
+  const [selected, setSelected] = useState("");
 
   const getCode = (e) => {
     var finalCode = "";
@@ -21,8 +26,31 @@ const Code = () => {
     setCode(getCode(e));
   };
 
-  const onClick = () => {
+  const onClick = async () => {
     // Send code to back-end
+    var res;
+    var input = {
+      value: base64.encode(code),
+    };
+    if (selected === "UML") {
+      res = await axios({
+        method: "post",
+        url: "localhost:5000/uml",
+        data: input,
+      });
+    } else if (selected === "Call Graph") {
+      res = await axios({
+        method: "post",
+        url: "localhost:5000/callgraph",
+        data: input,
+      });
+    }
+
+    img = base64.decode(res.status);
+  };
+
+  const handleSelection = (e) => {
+    setSelected(e.target.value);
   };
   return (
     <div>
@@ -31,10 +59,17 @@ const Code = () => {
         <span>Get</span>
       </button>
 
-      <select className={clsx(styles.dropdown)} name="graphs" id="graphs">
+      <select
+        className={clsx(styles.dropdown)}
+        onChange={handleSelection}
+        name="graphs"
+        id="graphs"
+      >
         <option value="UML">UML</option>
         <option value="Call Graph">Call Graph</option>
       </select>
+
+      {img && <img src={`data:image/png;base64,${img}`} />}
     </div>
   );
 };
